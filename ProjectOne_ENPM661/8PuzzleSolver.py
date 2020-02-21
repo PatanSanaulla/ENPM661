e4fa1ffbc300ed1eb265543cfa21bd0db9ba0494
PatanSanaulla <spatankhan@gmail.com>
2020-02-20 23:55:22 -0800

    modified the method names and format as per the PPT

--- ProjectOne_ENPM661/8PuzzleSolver.py
+++ ProjectOne_ENPM661/8PuzzleSolver.py
@@ -3,12 +3,11 @@ import copy as cp
 from datetime import datetime
 import sys
 
-
-#print(datetime.now())
+print(datetime.now())
 REQUIRED_RESULT = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-STEPS = []
-NODE_QUEUE = []
-TREE_QUEUE = []
+STEPS = [] #list of 8 puzzle steps backtracked in list format []
+MATRIX_8PUZZLE_NODES = [] #List of Node objects based on the order of generation.  
+TREE_QUEUE = [] #List of cube tile values for all the created nodes
 
 class Node:
 
@@ -22,16 +21,16 @@ class Node:
 		self.OLocation_x = None
 		self.OLocation_y = None
 		self.cube = cube
-		self.getOLocation()
+		self.blankTileLocation()
 		self.addToTree()
 
-	#Method of Node Class, to check the inversion of the cube formation and add the node to tree
+	#addToTree - Method of Node Class, to check the inversion of the cube formation and add the node to tree
 	#self: Object of Node
 	def addToTree(self):
-		OneD_cube = list((np.array(self.cube)).ravel())
-		OneD_cube.remove(0)
+		OneD_cube = list((np.array(self.cube)).ravel()) #making the 2D array into list
+		OneD_cube.remove(0) #removing the 0 to perform the solvabliity check 
 
-		inv_count = 0
+		inv_count = 0 #inversion counter
 		for i in range(8):
 			for j in range (i+1,8):
 				if (OneD_cube[i]>OneD_cube[j]):
@@ -42,84 +41,79 @@ class Node:
 		else:
 			self.isSolvable = False
 		TREE_QUEUE.append(self.cube)
-		NODE_QUEUE.append(self)
+		MATRIX_8PUZZLE_NODES.append(self)
 
 	#Method of Node Class, to check if the newCube formed already exists in the tree else create a new node
 	#self: Object of Node
 	#newCube: 2D array of a new cube formation
 	def checkAndCreateNode(self, newCube):
 		if newCube in TREE_QUEUE:
-			TREE_QUEUE.append("REPEAT")
-			NODE_QUEUE.append("REPEAT")
+			#To Check if the node value is already in tree
+			pass
 		else:
 			Node(self, newCube)
 
-	#Method of Node Class, to generate the Moves
+	#generateMoves - Method of Node Class, to generate the Moves
 	#self: Object of Node
 	def generateMoves(self):
-		self.moveDown()
+		self.actionMoveDown()
 
-	#Method of Node Class, to save the 0 Location in the Node
+	#blankTileLocation - Method of Node Class, to save the 0 Location in the Node
 	#self: Object of Node
-	def getOLocation(self):
+	def blankTileLocation(self):
 		for x, i in enumerate(self.cube):
 			for y, j in enumerate(i):
 				if j == 0:
 					self.OLocation_x = x
 					self.OLocation_y = y 
 
-	#Method of Node Class, to generate the formation of the cube when the 0 moves down and appends to the Nodes queue
+	#actionMoveDown - Method of Node Class, to generate the formation of the cube when the 0 moves down and appends to the Nodes queue
 	#self: Object of Node
-	def moveDown(self):
+	def actionMoveDown(self):
 		if(self.OLocation_x != 2): #The 0 is not in the last row
 			newCube = cp.deepcopy(self.cube)
 			newCube[self.OLocation_x+1][self.OLocation_y], newCube[self.OLocation_x][self.OLocation_y] = newCube[self.OLocation_x][self.OLocation_y], newCube[self.OLocation_x+1][self.OLocation_y]
 			self.checkAndCreateNode(newCube)
 			if newCube != REQUIRED_RESULT:
-				self.moveRight()
+				self.actionMoveRight()
 		else:
-			NODE_QUEUE.append(None)
-			self.moveRight()
+			self.actionMoveRight()
 
-	#Method of Node Class, to generate the formation of the cube when the 0 moves right and appends to the Nodes queue
+	#actionMoveRight - Method of Node Class, to generate the formation of the cube when the 0 moves right and appends to the Nodes queue
 	#self: Object of Node
-	def moveRight(self):
+	def actionMoveRight(self):
 		if(self.OLocation_y != 2): #The 0 is not in the last coloumn
 			newCube = cp.deepcopy(self.cube)
 			newCube[self.OLocation_x][self.OLocation_y+1], newCube[self.OLocation_x][self.OLocation_y] = newCube[self.OLocation_x][self.OLocation_y], newCube[self.OLocation_x][self.OLocation_y+1]
 			self.checkAndCreateNode(newCube)
 			if newCube != REQUIRED_RESULT:
-				self.moveLeft()
+				self.actionMoveLeft()
 		else:
-			NODE_QUEUE.append(None)
-			self.moveLeft()
+			self.actionMoveLeft()
 
-	#Method of Node Class, to generate the formation of the cube when the 0 moves left and appends to the Nodes queue
+	#actionMoveLeft - Method of Node Class, to generate the formation of the cube when the 0 moves left and appends to the Nodes queue
 	#self: Object of Node
-	def moveLeft(self):
+	def actionMoveLeft(self):
 		if(self.OLocation_y != 0): #The 0 is not in the first coloumn
 			newCube = cp.deepcopy(self.cube)
 			newCube[self.OLocation_x][self.OLocation_y-1], newCube[self.OLocation_x][self.OLocation_y] = newCube[self.OLocation_x][self.OLocation_y], newCube[self.OLocation_x][self.OLocation_y-1]
 			self.checkAndCreateNode(newCube)
 			if newCube != REQUIRED_RESULT:
-				self.moveUp()
+				self.actionMoveUp()
 		else:
-			NODE_QUEUE.append(None)
-			self.moveUp()
+			self.actionMoveUp()
 
-	#Method of Node Class, to generate the formation of the cube when the 0 moves up and appends to the Nodes queue
+	#actionMoveUp - Method of Node Class, to generate the formation of the cube when the 0 moves up and appends to the Nodes queue
 	#self: Object of Node
-	def moveUp(self):
+	def actionMoveUp(self):
 		if(self.OLocation_x != 0): #The 0 is not in the first row
 			newCube = cp.deepcopy(self.cube)
 			newCube[self.OLocation_x-1][self.OLocation_y], newCube[self.OLocation_x][self.OLocation_y] = newCube[self.OLocation_x][self.OLocation_y], newCube[self.OLocation_x-1][self.OLocation_y]
 			self.checkAndCreateNode(newCube)
-		else:
-			NODE_QUEUE.append(None)
 
 
 
-#Method to generate the path to the root node from the selected node
+#generatePath - Method to generate the path to the root node from the selected node
 #fromNode: object of class Node
 def generatePath(fromNode):
 	while fromNode != None:
@@ -127,46 +121,42 @@ def generatePath(fromNode):
 		fromNode = fromNode.parent
 
 
-#Method to Write the steps or the cube formation from the Given formation to the Required state
+#printPath - Method to Write the steps or the cube formation from the Given formation to the Required state
 def printPath():
 	if(len(STEPS) == 0):
-		f_nodePath.write("-----------------------------------\n")
 		f_nodePath.write("NOT A SOLVALBLE ENTRY")
-		print("NOT A SOLVALBLE ENTRY")
+		exitPuzzle("NOT A SOLVALBLE ENTRY")
 	else:
-		for x in reversed(STEPS):
-			f_nodePath.write("-----------------------------------")
-			f_nodePath.write("\n")
+		for step in reversed(STEPS):
 			try:
-				for line in x:
-					f_nodePath.write(" ".join([str(i) for i in line]))
-					f_nodePath.write("\n")
+				coloumn_wise = ""
+				for col in range(3):
+					coloumn_wise = coloumn_wise +" "+(" ".join([str(step[row][col]) for row in range(3)]))
+				f_nodePath.write(coloumn_wise)
+				f_nodePath.write("\n")
 			except AttributeError:
 				f_nodePath.write("NONE\n")
 			except TypeError:
 				f_nodePath.write("NONE\n")
 				continue
 
-#Method to Write the All the cube formation from the Given formation to the Required state
-def printNodes():
-	f_nodes.write(" TOTAL NUMBER OF NODES: %d \n"%len(NODE_QUEUE))
-	for i in NODE_QUEUE:
-		f_nodes.write("----------------------------- \n")
+#printNodesandInfo- Method to Write the All the cube formation from the Given formation to the Required state
+def printNodesandInfo():
+	for node in MATRIX_8PUZZLE_NODES:
 		try:
-			if i == None:
-				f_nodes.write(" NOT POSSIBLE MOVE \n")
-			else:
-				if i == "REPEAT":
-					f_nodes.write(" REPEATED NODE \n")
-				else:
-					if i.cube:
-						for line in i.cube:
-							f_nodes.write(" ".join([str(val) for val in line]))
-							f_nodes.write("\n")
+			if node.cube:
+				coloumn_wise = ""
+				for col in range(3):
+					coloumn_wise = coloumn_wise +" "+(" ".join([str(node.cube[row][col]) for row in range(3)]))
+				f_nodes.write(coloumn_wise)
+				f_nodes.write("\n")
+
+				f_nodeInfo.write(str(MATRIX_8PUZZLE_NODES.index(node))+" "+str(MATRIX_8PUZZLE_NODES.index(node.parent))+" "+'0')
+				f_nodeInfo.write("\n")
 		except:
 			continue
 
-#Method to check if the given cube formation values are valid
+#validEntry- Method to check if the given cube formation values are valid
 #givenValue - an list of integer values
 #returns - True if valid else False
 def validEntry(givenValue):
@@ -178,13 +168,14 @@ def validEntry(givenValue):
 	else:
  		return False
 
-#Method to print the error or success message and exit the puzzle 
+#exitPuzzle- Method to print the error or success message and exit the puzzle 
 #Message - String, value of the message
 def exitPuzzle(Message):
 	print(Message)
 	f_nodes.close()
 	f_nodePath.close()
-	#print(datetime.now())
+	f_nodeInfo.close()
+	print(datetime.now())
 	sys.exit()
 
 
@@ -192,13 +183,18 @@ def exitPuzzle(Message):
 try:
 	f_nodePath = open("nodePath.txt","w")
 	f_nodes = open("Nodes.txt","w")
+	f_nodeInfo = open("nodeInfo.txt","w")
 	givenCube = list(map(int, sys.argv[1].split()))
 	cubeFormat = []
 
 	if validEntry(givenCube) == True:
-		for x in range(0,9,3):
-			sliceObj = slice(x, x+3)
-			cubeFormat.append(givenCube[sliceObj])
+		#Reading from Coloumn Arrangement to Row Arrangement
+		for i in range(3):
+			newRow = []
+			for j in range (i,9,3):
+				newRow.append(givenCube[j])
+			cubeFormat.append(newRow)
+		#Creating the root node with the given value of tiles	
 		root = Node(None, cubeFormat)
 	else:
 		raise ValueError
@@ -208,11 +204,10 @@ except ValueError:
 	exitPuzzle("INVALID ENTRY")
 
 #To expand the tree further inorder to reach the required node
-for topNode in NODE_QUEUE:
+for topNode in MATRIX_8PUZZLE_NODES:
 	try:
 		if(topNode.cube == REQUIRED_RESULT):
 			generatePath(topNode)
-			printPath()
 			break
 		else:
 			if topNode.isSolvable == True:
@@ -222,6 +217,6 @@ for topNode in NODE_QUEUE:
 	except AttributeError:
 		continue
 
-printNodes()
+printNodesandInfo()
 printPath()
 exitPuzzle("FOUND SOLUTION!!!")
